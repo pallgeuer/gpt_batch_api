fb2d76bc290a7ad6f9fe882a7e4933866f132492
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2024-10-22 16:28:40 +0200

    WIP on GPT requester

--- gpt_batch_api.py
+++ gpt_batch_api.py
@@ -5,10 +5,15 @@
 #  - The current remote files can be manually managed at: https://platform.openai.com/storage
 #  - The current remote batches can be manually managed at: https://platform.openai.com/batches
 
+# TODO: Organise and structure this file (e.g. utils.py)
+
 # Imports
+import os
 import math
 import time
 import logging
+import contextlib
+import dataclasses
 from typing import Optional, Iterable, Self, Any, Protocol, ContextManager
 import tqdm
 import filelock
@@ -81,4 +86,94 @@ class LockFile:
 		self.lock.release()
 		return False
 
+# GPT request class
+@dataclasses.dataclass(frozen=True)
+class GPTRequest:
+	payload: dict[str, Any]         # Request payload (JSON-like)
+	meta: Optional[dict[str, Any]]  # Optional custom metadata to associate with this request (JSON-like)
+
+# GPT requester class
+class GPTRequester:
+
+	def __init__(self, working_dir: str, autocreate_working_dir: bool = True, name_prefix: str = 'gpt_requester', lock_timeout: float = -1):
+		# TODO: DOC
+
+		self.working_dir = os.path.abspath(working_dir)
+		working_dir_parent = os.path.dirname(self.working_dir)
+		if not os.path.isdir(working_dir_parent):
+			raise ValueError(f"Parent directory of GPT working directory needs to exist: {working_dir_parent}")
+
+		self.name_prefix = name_prefix
+		if not self.name_prefix:
+			raise ValueError("Name prefix cannot be empty")
+
+		self.autocreate_working_dir = autocreate_working_dir
+		created_working_dir = False
+		if self.autocreate_working_dir:
+			with contextlib.suppress(FileExistsError):
+				os.mkdir(self.working_dir)
+				created_working_dir = True
+
+		log.info(f"Using GPT requester of prefix '{self.name_prefix}' in dir: {self.working_dir}{' [CREATED]' if created_working_dir else ''}")
+		if not os.path.isdir(self.working_dir):
+			raise FileNotFoundError(f"GPT working directory does not exist: {self.working_dir}")
+
+		self.lock_timeout = lock_timeout
+		self.lock_file = os.path.join(self.working_dir, f"{self.name_prefix}.lock")
+		self.state_file = os.path.join(self.working_dir, f"{self.name_prefix}_state.json")
+		self.requests_file = os.path.join(self.working_dir, f"{self.name_prefix}_requests.json")
+		self.batch_file_prefix = os.path.join(self.working_dir, f"{self.name_prefix}_batch_")
+
+		self._enter_stack = contextlib.ExitStack()
+		self.lock = LockFile(lock_file=self.lock_file)
+		self.state: Optional[dict[str, Any]] = None
+		self.requests = None  # TODO: requests should be a dataclass pair of lists corresponding to those requests that are currently on disk in the file, and temporary ones that have been added in code at runtime but not saved yet
+
+	def __enter__(self) -> Self:
+		with self._enter_stack as stack:
+			stack.enter_context(self.lock)
+			with utils.DelayKeyboardInterrupt():
+				stack.callback(self.unload_state_file)  # TODO: Just set some state members to None etc
+				try:
+					self.load_state_file()
+				except FileNotFoundError:
+					self.create_state_file()
+				stack.callback(self.unload_requests_file)  # TODO: Just set some state members to None etc
+				try:
+					self.load_requests_file()
+				except FileNotFoundError:
+					self.create_requests_file()
+			self._enter_stack = stack.pop_all()
+		assert self._enter_stack is not stack
+		return self
+
+	def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+		return self._enter_stack.__exit__(exc_type, exc_val, exc_tb)
+
+	def create_state_file(self):
+		# TODO: Further implement load_state_file, unload_state_file, create_requests_file, load_requests_file, unload_requests_file
+		self.state = dict(  # TODO: Dataclass-based
+			version=1,
+			batches=[],
+		)
+
+
+
+	# TODO: No exception (KeyboardInterrupt or Error) should leave the disk state in an inconsistent state (cannot really guarantee that when saving state/requests files sequentially?)
+	# TODO: Load and sanity check the state file if it exists, otherwise create one, Load and sanity check the requests file (dual file contents vs added on top lists in memory) if it exists (error to exist if no state file existed), otherwise create an empty one
+
+	@contextlib.contextmanager
+	def add_request(self, req: GPTRequest) -> ContextManager[bool]:  # TODO: Auto-true flag whether to send off complete batches if possible, or literally just add it to the requests JSONL?
+		# TODO: Need to yield whether any state actually changed yet?
+		yield
+
+	@contextlib.contextmanager
+	def add_requests(self, reqs: Iterable[GPTRequest]) -> ContextManager[None]:  # TODO: Auto-true flag whether to send off complete batches if possible, or literally just add it to the requests JSONL?
+		# TODO: Can only iterate Iterable ONCE!!!
+		yield
+
+	# TODO: Version
+	# TODO: Metrics (tokens in/out (what happens about thought tokens), per batch, num requests, divided by successful/unsuccessful)
+	# TODO: Wandb
+	# TODO: Remove tqdm if it is not actually used (would be better for wandb logs)
 # EOF
