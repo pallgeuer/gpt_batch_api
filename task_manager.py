d401e3211cf68379483f3d3d8ee7f35b42d2a340
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2024-11-01 19:27:01 +0100

    WIP on task manager

--- task_manager.py
+++ task_manager.py
@@ -1,7 +1,76 @@
-# Task manager (wraps GPT requester to include the source data side of the pipeline as well)
+# Task manager (wraps a GPT requester to include the sample generation side of the pipeline as well)
 
 # Imports
-from . import gpt_requester
+import os
+import copy
+import contextlib
+import dataclasses
+from typing import Any, Optional, Self
+from .logger import log
+from . import gpt_requester, utils
+
+#
+# Task state file
+#
+
+# Task state class
+@dataclasses.dataclass
+class TaskState:
+	version: int = 1                                                             # Data format version number
+	meta: dict[str, Any] = dataclasses.field(default_factory=dict)               # Task-level metadata
+	committed_meta: dict[str, Any] = dataclasses.field(default_factory=dict)     # Task-level information about the samples committed so far (arbitrary key-value store)
+	committed_samples: dict[str, Any] = dataclasses.field(default_factory=dict)  # Sample-level information about the samples committed so far (maps sample keys to corresponding JSON-compatible data)
+	completed_meta: dict[str, Any] = dataclasses.field(default_factory=dict)     # Task-level information about the samples completed so far (arbitrary key-value store)
+	completed_samples: dict[str, Any] = dataclasses.field(default_factory=dict)  # Sample-level information about the samples completed so far (maps sample keys to corresponding JSON-compatible data)
+
+# Task state file class
+class TaskStateFile:
+
+	state: Optional[TaskState]
+
+	def __init__(self, path: str, init_meta: Optional[dict[str, Any]]):
+		# path = Path to the JSON task state file to load/save/manage (nominally *.json extension)
+		# init_meta = Value to initialise the meta field with if the task state file is newly created (deep copy on create)
+		self.path = os.path.abspath(path)
+		log.info(f"Task state file: {self.path}")
+		self.name = os.path.basename(self.path)
+		self.init_meta = init_meta if init_meta is not None else {}
+		self._enter_stack = contextlib.ExitStack()
+		self.state = None
+
+	def __enter__(self) -> Self:
+		with self._enter_stack as stack:
+			stack.callback(self.unload)
+			try:
+				self.load()
+			except FileNotFoundError:
+				self.create()
+			assert self.state is not None
+			self._enter_stack = stack.pop_all()
+		assert self._enter_stack is not stack
+		return self
+
+	def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+		return self._enter_stack.__exit__(exc_type, exc_val, exc_tb)
+
+	def create(self):
+		self.state = TaskState(meta=copy.deepcopy(self.init_meta))
+		self.save(stack=None)
+
+	def load(self):
+		with open(self.path, 'r', encoding='utf-8') as file:
+			file_size = utils.get_file_size(file)
+			self.state = utils.dataclass_from_json(cls=TaskState, json_data=file)
+		log.info(f"Loaded task state file with {len(self.state.committed_samples)} committed and {len(self.state.completed_samples)} completed samples ({utils.format_size(file_size)}): {self.name}")
+
+	def save(self, stack: Optional[contextlib.ExitStack[Optional[bool]]]):
+		with utils.SafeOpenForWrite(self.path, stack=stack) as file:
+			utils.json_from_dataclass(obj=self.state, file=file)
+			file_size = utils.get_file_size(file)
+		log.info(f"Saved task state file with {len(self.state.committed_samples)} committed and {len(self.state.completed_samples)} completed samples ({utils.format_size(file_size)}): {self.name}")
+
+	def unload(self):
+		self.state = None
 
 #
 # Task manager class
@@ -10,8 +79,50 @@ from . import gpt_requester
 # Task manager class
 class TaskManager:
 
-	def __init__(self):
-		pass  # TODO: Output file MUST be _output*.EXT
+	# Construct a task manager to make use of the OpenAI Batch API to process samples
+	def __init__(
+		self,
+		task_dir: str,                               # Path to the task working directory to use (will be used for automatically managed lock, state, requests, batch, task state, and output files)
+		name_prefix: str,                            # Name prefix to use for all files created in the task working directory (e.g. 'my_task')
+		*,                                           # Keyword arguments only beyond here
+		init_meta: Optional[dict[str, Any]] = None,  # Value to initialise the task state meta field with if the task state file is newly created (deep copy on create)
+		**gpt_requester_kwargs,                      # Keyword arguments to be passed on to the internal GPTRequester instance
+	):
 
-	# TODO: Generic TaskOutputFile (could be simple JSON, or potentially chunked JSONL, or totally different file ext?)
+		self.GR = gpt_requester.GPTRequester(working_dir=task_dir, name_prefix=name_prefix, **gpt_requester_kwargs)
+		self.task = TaskStateFile(path=os.path.join(self.GR.working_dir, f"{self.GR.name_prefix}_task.json"), init_meta=init_meta)
+		# TODO: Output file MUST be _output*.EXT => Class to generically wrap what kind of output file? Or just a method override (would this require frequent duplication of boiler plate code for saving e.g. just a standard JSON)? / Generic TaskOutputFile (could be simple JSON, or potentially chunked JSONL, or totally different file ext?)
+
+		self._enter_stack = contextlib.ExitStack()
+		self.T: Optional[TaskState] = None
+
+	# Run the task manager to completion
+	def run(self):
+		with self:
+			raise NotImplementedError  # TODO: Is it possible to have this be a while True: step(); wait() where step() does everything it can up to the point where it would have to wait for a batch to complete?
+
+	# Enter method for the required use of TaskManager as a context manager
+	def __enter__(self) -> Self:
+		with self._enter_stack as stack:
+			stack.enter_context(self.GR)
+			stack.callback(self.on_exit)
+			stack.enter_context(self.task)
+			self.T = self.task.state
+			self.validate_state()
+			self._enter_stack = stack.pop_all()
+		assert self._enter_stack is not stack
+		return self
+
+	# Exit method for the required use of TaskManager as a context manager
+	def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+		return self._enter_stack.__exit__(exc_type, exc_val, exc_tb)
+
+	# Local actions to perform on exit
+	def on_exit(self):
+		self.T = None
+
+	# Validate that there are no obvious issues with the current state
+	def validate_state(self):
+		if not self.T.committed_samples.keys() >= self.T.completed_samples.keys():
+			raise ValueError(f"Unexpected completed yet not committed samples: {sorted(self.T.completed_samples.keys() - self.T.committed_samples.keys())}")
 # EOF
--- task_manager_demo.py
+++ task_manager_demo.py
@@ -5,35 +5,64 @@ import os
 import sys
 import logging
 import argparse
-from . import task_manager
+from typing import Sequence
+from . import gpt_requester, task_manager, utils
 
 # TODO: Add any new demos to gpt_batch_api/commands.txt
 # TODO: Demo a basic single opinion/single stage text writing task with simple JSON output
 # TODO: Demo a multi-opinion task
 # TODO: Demo a multi-stage task (continuing the conversation after first response)
-# TODO: One demo should have chunked JSONL output
+# TODO: One demo should have chunked JSONL output (size- and/or line-limited possible)
+# TODO: At least one demo should use structured outputs, and at least one shouldn't
+# TODO: Direct is tested by having an auto-direct thing when an odd small number of samples left at the end (make sure the numbers are such that a small thing is left over)
 
 #
 # Demo: Character codes
 #
 
+# Character codes task class
+class CharCodesTask(task_manager.TaskManager):
+
+	def __init__(self, cfg: utils.Config, task_dir: str, char_ranges: Sequence[tuple[int, int]]):
+		super().__init__(
+			task_dir=task_dir,
+			name_prefix=cfg.task_prefix,
+			init_meta=dict(model=cfg.model),
+			**gpt_requester.GPTRequester.get_kwargs(cfg),
+		)
+		self.char_ranges = char_ranges  # TODO: Recall that you still want to ignore non-printable characters: char = chr(code_point); if char.isprintable(): PROCESS sample
+		# # TODO: Recall that ultimately you should only add requests for samples (code points) that haven't been committed yet! UNLESS they also completed and FAILED => How to deal with that??
+
+
+
+
+	# # TODO: State what the unicode character is, give a one sentence description of what the symbol represents and where it comes from, give a sample sentence including the character at least once (if not included then this is a 'parse error' situation, i.e. soft fail)
+	#
+	# # Create a GPT requester to manage getting information about unicode characters
+	#
+	# # TODO: _task.json file for storing basically information about what you've already processed (must be hashable after lists are converted to tuples and dicts are converted to tuples of items?)
+	# # TODO: _output.json OR _data_001.jsonl file for storing the ACTUAL output/processed response data (NOT needed to determine what samples are needed in future)
+	#
+	# # Enter the GPT requester and thereby allow it to obtain a lock on the state files
+	# with gpt_requester:
+	# 	# Hard-code the unicode characters we are interested in getting information about (we restrict these ranges further to printable characters)
+	#
+	# 	# TODO: Sample key should uniquely identify the VALUE of the sample in some explicit/implicit way (so that if I change my set of noun candidates, it just automatically does whichever ones are new and leaves everything else untouched)
+	#
+	# 	# TODO: DOC, check which actually still need to be done
+	# 	code_point_a, code_point_b = unicode_blocks[0]
+	# 	for code_point, char in ((cp, chr(cp)) for cp in range(code_point_a, code_point_b + 1) if chr(cp).isprintable()):
+	# 		response = gpt_requester.direct_request(TODO)
+
+
+
+
 # Demonstrate the task manager class on the task of generating information about unicode characters
-def demo_char_codes():
-
-	# TODO: State what the unicode character is, give a one sentence description of what the symbol represents and where it comes from, give a sample sentence including the character at least once (if not included then this is a 'parse error' situation, i.e. soft fail)
-	# TODO: Only allow printable/new characters
-
-	# Create a GPT requester to manage getting information about unicode characters
-	working_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tasks')
-	gpt_requester = GPTRequester(working_dir=working_dir, name_prefix='demo_char_codes')
-
-	# TODO: _task.json file for storing basically information about what you've already processed (must be hashable after lists are converted to tuples and dicts are converted to tuples of items?)
-	# TODO: _output.json OR _data_001.jsonl file for storing the ACTUAL output/processed response data (NOT needed to determine what samples are needed in future)
-
-	# Enter the GPT requester and thereby allow it to obtain a lock on the state files
-	with gpt_requester:
-		# Hard-code the unicode characters we are interested in getting information about (we restrict these ranges further to printable characters)
-		unicode_blocks = (
+def demo_char_codes(cfg: utils.Config, task_dir: str):
+	CharCodesTask(
+		cfg=cfg,
+		task_dir=task_dir,
+		char_ranges=(
 			(0x0000, 0x007F),  # Basic Latin
 			(0x0080, 0x00FF),  # Latin-1 Supplement
 			(0x0100, 0x017F),  # Latin Extended-A
@@ -43,18 +72,8 @@ def demo_char_codes():
 			(0x0370, 0x03FF),  # Greek and Coptic
 			(0x0400, 0x04FF),  # Cyrillic
 			(0x0500, 0x052F),  # Cyrillic Supplement
-		)
-
-		# TODO: DOC, check which actually still need to be done
-		code_point_a, code_point_b = unicode_blocks[0]
-		for code_point, char in ((cp, chr(cp)) for cp in range(code_point_a, code_point_b + 1) if chr(cp).isprintable()):
-			response = gpt_requester.direct_request(TODO)
-
-	# TODO: Direct
-	# TODO: add_request
-	# TODO: add_requests
-	# TODO: add+commit simultaneous
-	pass  # TODO for code_point in range(0x0300): char = chr(code_point); if char.isprintable(): PROCESS sample
+		),
+	).run()
 
 #
 # Run
@@ -66,12 +85,22 @@ def main():
 	logging.basicConfig(level=logging.INFO, format="[%(levelname)s][%(asctime)s] %(message)s", handlers=[logging.StreamHandler(sys.stdout)])
 
 	parser = argparse.ArgumentParser(description="Demonstrate the TaskManager class with example applications")
-	parser.add_argument('--task', type=str, help='Demo task to run')
-	# TODO: Demonstrate default argparse use to supply configs (also allow custom default values with fallback to default default-values)
-	args = parser.parse_args()
+	parser.add_argument('--task', type=str, required=True, help='Which task to run (e.g. char_codes)')
+	parser.add_argument('--task_prefix', type=str, help='Name prefix to use for task-related files')
 
+	parser_common = parser.add_argument_group('Common')
+	parser_common.add_argument('--model', type=str, default='gpt-4o-mini', help="Model to use for new tasks")
+
+	# TODO: GPT requester argument group: Demonstrate default argparse use to supply configs (also allow custom default values with fallback to default default-values)
+	# TODO: Also populate gpt_batch_api.yaml for the Hydra alternative (will need it later when using gpt_batch_api from nounnetdev)
+	gpt_requester.GPTRequester.configure_argparse(parser=parser)
+	args = parser.parse_args()
+	if args.task_prefix is None:
+		args.task_prefix = args.task
+
+	task_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tasks')
 	if args.task == 'char_codes':
-		demo_char_codes()
+		demo_char_codes(cfg=args, task_dir=task_dir)
 	elif args.task is None:
 		raise ValueError("Please specify which task to demo using --task")
 	else:
